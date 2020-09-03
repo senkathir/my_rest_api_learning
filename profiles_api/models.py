5deb51ec523ede560525ebd18c9f2c4ad94c1821
Senkathir Selvan <Senkathir.Selvan@tatacommunications.com>
2020-09-03 23:44:00 +0530

    Custom user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self, email, name, password=None):#passwd can be none also
+        """Create new user profile"""
+        if not email:
+            raise ValueError('User must have an email id')
+        email = self.normalize_email(email)#normalize the frst half of email i.e. case sensitive and second half will be case insensitive
+        user = self.model(email=email,name=name)#creates new model for this manager with the input args
+        user.set_password(password)#uses AbstractBaseUser for password hashing (using set_password)
+        user.save(using=self._db)#save to db
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create superuser"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True#from PermissionsMixin
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """DB model for users"""
+    email = models.EmailField(max_length=255, unique=True)#creates a "email" column in userprofile table
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()#for managing this model which overrides the default user model   #Custom manager for overriding default
+    USERNAME_FIELD = 'email'#instead of default login with username, use email in place of username
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retreive full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retreive short name of user"""
+        return self.name
+    def __str__(self):
+        """String reprensentation of model"""
+        return self.email#we ll usually specify the field with which we will identify the model
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'#use this model for user auth..i.e. overriding default user auth
